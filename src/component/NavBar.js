1d9d093ba5880694a61376f2bc8f018031d6414b
SAURABH KUMAR <hmsaurabhkr@gmail.com>
2022-08-06 19:21:15 +0530

    fir nav bar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -5,156 +5,95 @@ import NavLinkDropdown from "./ManagePropertyDropdown";
 import ResourceDropdown from "./ResourceDropdown";
 
 function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
   return (
     <div className="sticky top-0 z-50 ">
-      <nav className="bg-white ">
-        <div className="mx-auto  px-4 sm:px-6 md:px-8">
-          <div className="flex h-16 items-center  justify-between ">
-            <div className="flex items-center justify-end ">
+      <nav className="w-full bg-white shadow">
+        <div className="mx-auto justify-between px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
+          <div>
+            <div className="flex items-center justify-between py-3 md:block md:py-5">
               <div className="flex space-x-3">
                 <BiBuildingHouse className="h-8 w-8 text-purple-800" />
                 <span className="self-center whitespace-nowrap text-2xl font-semibold text-purple-900 ">
                   Rent a Property
                 </span>
               </div>
-              <div className="hidden lg:hidden xl:block ">
-                <div className="ml-10 flex items-baseline justify-between space-x-5 text-gray-900 ">
-                  <a
-                    href="/"
-                    className=" rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700 "
-                  >
-                    Rent
-                  </a>
-                  <a
-                    href="/"
-                    className=" rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700 "
-                  >
-                    Buy
-                  </a>
-                  <a
-                    href="/"
-                    className=" rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700 "
-                  >
-                    Sell
-                  </a>
-                  <div>
-                    <NavLinkDropdown />
-                  </div>
-                  <div>
-                    <ResourceDropdown />
-                  </div>
-                </div>
+              <div className="md:hidden ">
+                <button
+                  className="rounded-md p-2 text-gray-700 outline-none focus:border focus:border-gray-400"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
-            <div className="hidden lg:hidden xl:block ">
-              <button
-                type="button"
-                className="mr-2 mb-2 rounded-lg px-5 py-2.5 text-sm font-medium text-purple-700 outline outline-1 outline-purple-400 hover:bg-purple-200  "
-              >
-                Login
-              </button>
-              <button
-                type="button"
-                className="mr-2 mb-2 rounded-lg bg-purple-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-700  "
-              >
-                Sign Up
-              </button>
-            </div>
-
-            {/* Nav menu for small screen */}
-
-            <div className="-mr-2 flex lg:block xl:hidden">
-              <button
-                href="/"
-                onClick={() => setIsOpen(oldState => !oldState)}
-                type="a"
-                className="inline-flex items-center justify-center rounded-md bg-purple-200 p-2 text-gray-900 hover:text-purple-700  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
           </div>
-        </div>
-
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterhref="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leavehref="opacity-0 scale-95"
-        >
-          {ref => (
-            <div className="" id="mobile-menu">
-              <div
-                ref={ref}
-                className="space-y-1 px-2 pt-2 pb-3 text-gray-900 sm:px-3"
-              >
+          <div>
+            <div
+              className={`mt-8 flex-1 justify-self-center pb-3 md:mt-0 md:block md:pb-0 ${
+                navbar ? "block" : "hidden"
+              }`}
+            >
+              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                <li className="block rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700">
                 <a
                   href="/"
-                  className=" block rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700"
+                  className=" "
                 >
                   Rent
                 </a>
-
+                </li>
+                <li className="block rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700">
                 <a
                   href="/"
-                  className="block rounded-md px-3 py-2 text-base font-medium hover:bg-purple-200 hover:text-purple-700"
+                  className=""
                 >
                   Buy
                 </a>
-
+                </li>
+                <li className="block rounded-md px-3 py-2 text-sm font-medium hover:bg-purple-200 hover:text-purple-700">
                 <a
                   href="/"
-                  className="block  rounded-md px-3 py-2 text-base font-medium  hover:bg-purple-200 hover:text-purple-700"
+                  className=""
                 >
                   Sell
                 </a>
-                <div>
-                  <NavLinkDropdown />
-                </div>
-                <div>
-                  <ResourceDropdown />
-                </div>
-              </div>
-              <div className=" px-5  lg:hidden  ">
+                </li>
+                <li className="">
+                <NavLinkDropdown />
+                </li>
+                <li className="">
+                <ResourceDropdown />
+                </li>
+                <li className="">
                 <button
                   type="button"
                   className="mr-2 mb-2 rounded-lg px-5 py-2.5 text-sm font-medium text-purple-700 outline outline-1 outline-purple-400 hover:bg-purple-200  "
@@ -167,10 +106,11 @@ function NavBar() {
                 >
                   Sign Up
                 </button>
-              </div>
+                </li>
+              </ul>
             </div>
-          )}
-        </Transition>
+          </div>
+        </div>
       </nav>
     </div>
   );
